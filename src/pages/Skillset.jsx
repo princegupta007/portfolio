85cdc5b6bcc17b638964b6c83f62c5d59513e2e7
Prince Gupta <princegupta98299@gmail.com>
2024-07-09 21:25:57 +0530

    leetcodeHeatmap

--- src/pages/Skillset.jsx
+++ src/pages/Skillset.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 import Particle from '../components/Particle'
 import Techstack from "../components/Skillset/Techstack";
 import Toolstack from "../components/Skillset/Toolstack";
-// import Leetcode from "../components/Skillset/Leetcode";
+import Leetcode from "../components/Skillset/Leetcode";
 import Github from "../components/Skillset/Github";
 
 const Skillset = () => {
@@ -21,7 +21,7 @@ const Skillset = () => {
         </h1>
         <Toolstack />
 
-        {/* <Leetcode /> */}
+        <Leetcode />
         <Github />
       </Container>
     </Container>
